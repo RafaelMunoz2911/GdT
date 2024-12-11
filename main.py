5845581e36d719d1a8e616ca548dd6a5ceea2347
Rafael Ortiz <rafa.ortiz2911@gmail.com>
2024-12-11 15:33:59 -0300

    Criando bt

--- main.py
+++ main.py
@@ -66,4 +66,41 @@ app_.place(x=50, y=7)
 app_linha = Label(frameCima, width=770, height=1, padx=5, anchor=NW, font=('Verdanda 1'), bg=co3, fg=co1)
 app_linha.place(x=0, y=47)
 
+#___________ menu
+img_contratos = Image.open('event.png')
+img_contratos = img_contratos.resize((18,18))
+img_contratos = ImageTk.PhotoImage(img_contratos)
+b_usuario = Button(frameEsquerda, image=img_contratos, compound=LEFT, anchor=NW, text=' Cadastrar um novo evento', bg= co4, fg=co1, font=('Ivy 11'), overrelief=RIDGE, relief=GROOVE)
+b_usuario.grid(row=0, column=0, sticky=NSEW, padx=5, pady=10)
+
+img_ativ = Image.open('task.png')
+img_ativ = img_ativ.resize((18,18))
+img_ativ = ImageTk.PhotoImage(img_ativ)
+b_usuario = Button(frameEsquerda, image=img_ativ, compound=LEFT, anchor=NW, text=' Cadastrar nova atividade', bg= co4, fg=co1, font=('Ivy 11'), overrelief=RIDGE, relief=GROOVE)
+b_usuario.grid(row=1, column=0, sticky=NSEW, padx=5, pady=10)
+
+img_terc = Image.open('terc.png')
+img_terc = img_terc.resize((18,18))
+img_terc = ImageTk.PhotoImage(img_terc)
+b_usuario = Button(frameEsquerda, image=img_terc, compound=LEFT, anchor=NW, text=' Cadastrar novo funcionario', bg= co4, fg=co1, font=('Ivy 11'), overrelief=RIDGE, relief=GROOVE)
+b_usuario.grid(row=2, column=0, sticky=NSEW, padx=5, pady=10)
+
+img_ver_terc = Image.open('verTerc.png')
+img_ver_terc = img_ver_terc.resize((18,18))
+img_ver_terc = ImageTk.PhotoImage(img_ver_terc)
+b_usuario = Button(frameEsquerda, image=img_ver_terc, compound=LEFT, anchor=NW, text=' Ver tos funcionários ', bg= co4, fg=co1, font=('Ivy 11'), overrelief=RIDGE, relief=GROOVE)
+b_usuario.grid(row=3, column=0, sticky=NSEW, padx=5, pady=10)
+
+img_presencas = Image.open('presen.png')
+img_presencas = img_presencas.resize((18,18))
+img_presencas = ImageTk.PhotoImage(img_presencas)
+b_usuario = Button(frameEsquerda, image=img_presencas, compound=LEFT, anchor=NW, text=' Gerar lista de presença', bg= co4, fg=co1, font=('Ivy 11'), overrelief=RIDGE, relief=GROOVE)
+b_usuario.grid(row=4, column=0, sticky=NSEW, padx=5, pady=10)
+
+img_pagamentos = Image.open('pag.png')
+img_pagamentos = img_pagamentos.resize((18,18))
+img_pagamentos = ImageTk.PhotoImage(img_pagamentos)
+b_usuario = Button(frameEsquerda, image=img_pagamentos, compound=LEFT, anchor=NW, text=' Registro de pagamentos', bg= co4, fg=co1, font=('Ivy 11'), overrelief=RIDGE, relief=GROOVE)
+b_usuario.grid(row=5, column=0, sticky=NSEW, padx=5, pady=10)
+
 janela.mainloop()
